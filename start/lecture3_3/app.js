ff48cae740c30a1a75adfba5407d08d300968701
Jack-Human <iketyke@gmail.com>
2021-11-03 18:46:40 +0000

    Update app.js

--- start/lecture3_3/app.js
+++ start/lecture3_3/app.js
@@ -93,7 +93,7 @@ class App{
 
 			const geometry = new THREE.BufferGeometry().setFromPoints( [
 				new THREE.Vector3(0,0,0),
-				new THREE.Vector3(0,0,-1),
+				new THREE.Vector3(0,0,-1)
 			]);
 			const line = new THREE.Line(geometry);
 			line.name = 'line';
